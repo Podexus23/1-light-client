1229aa35ea78f4ab72ef032ab14d127d0d107752
Podexus23 <santon23@gmail.com>
2025-10-23 17:59:13 +0300

    fix: tricky update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-console.log("hello pi");
 const serverBtn = document.getElementById("btn-server");
 const dbGetBtn = document.getElementById("btn-db");
 const dbSendBtn = document.getElementById("btn-db_res");
 const urlInput = document.getElementById("url");
 const p = document.getElementById("data-place");
+const p2 = document.getElementById("data-place2");
 
 serverBtn.addEventListener("click", async (e) => {
   const url = urlInput.value;
-  console.log(url);
   const res = await fetch(`${url}/api/light`);
   const data = await res.json();
   p.innerHTML = JSON.stringify(data);
@@ -15,7 +14,6 @@ serverBtn.addEventListener("click", async (e) => {
 
 dbGetBtn.addEventListener("click", async (e) => {
   const url = urlInput.value;
-  console.log(url);
   const res = await fetch(`${url}/api/light/check`);
   const data = await res.json();
   p.innerHTML = JSON.stringify(data);
@@ -23,8 +21,14 @@ dbGetBtn.addEventListener("click", async (e) => {
 
 dbSendBtn.addEventListener("click", async (e) => {
   const url = urlInput.value;
-  console.log(url);
   const res = await fetch(`${url}/api/light/send`);
   const data = await res.json();
   p.innerHTML = JSON.stringify(data);
 });
+
+setInterval(async () => {
+  const url = urlInput.value;
+  const res = await fetch(`${url}/api/light/send`);
+  const data = await res.json();
+  p2.innerHTML = JSON.stringify(data);
+}, 30000);
