1009895c97d26097642fd8ff07581ad852fc2083
Podexus23 <santon23@gmail.com>
2025-10-23 17:59:57 +0300

    fix: fixxing a fix

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ const urlInput = document.getElementById("url");
 const p = document.getElementById("data-place");
 const p2 = document.getElementById("data-place2");
 
+const url = "https://one-light-server.onrender.com";
+
 serverBtn.addEventListener("click", async (e) => {
   const url = urlInput.value;
   const res = await fetch(`${url}/api/light`);
@@ -27,7 +29,6 @@ dbSendBtn.addEventListener("click", async (e) => {
 });
 
 setInterval(async () => {
-  const url = urlInput.value;
   const res = await fetch(`${url}/api/light/send`);
   const data = await res.json();
   p2.innerHTML = JSON.stringify(data);
